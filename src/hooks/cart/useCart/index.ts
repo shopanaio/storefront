e032c2b3ac8fb83fc1fa3497dc6c7c586491349f
phl <philipp.sapronov@gmail.com>
2025-09-26 14:04:17 +0300

    refactor: enhance useCart hook with type definitions for improved type safety

--- src/hooks/cart/useCart/index.ts
+++ src/hooks/cart/useCart/index.ts
@@ -1,8 +1,16 @@
 import { cmsPick } from "@src/cms/pick";
 import useCartShopana from "./useCart.shopana";
 import useCartShopify from "./useCart.shopify";
+import { Entity } from "@src/entity";
+
+type UseCart = () => {
+  cart: Entity.Cart | null;
+  loading: boolean;
+  loaded: boolean;
+  error: Error | null;
+};
 
 export default cmsPick({
   shopana: useCartShopana,
   shopify: useCartShopify,
-});
+}) as UseCart;
