5706df6288a0f201d62101751e2b85292214771d
phl <philipp.sapronov@gmail.com>
2025-10-23 21:54:37 +0300

    feat: remove unnecessary hook

--- src/hooks/useBoxBuilderCategories.ts
+++
@@ -1,9 +0,0 @@
-import { BOX_BUILDER_CONFIG } from "@src/modules/box-builder/config/categories";
-
-export const useBoxBuilderCategories = () => {
-  return {
-    categories: BOX_BUILDER_CONFIG.step2.categories,
-    error: null,
-    loading: false,
-  };
-};
