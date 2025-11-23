942435af3d237a7beceb0c13a39a4f59fbd48264
phl <philipp.sapronov@gmail.com>
2025-11-23 17:12:05 +0200

    remove legacy

--- src/app/demo-framework/[[...slug]]/page.tsx
+++
@@ -1,5 +0,0 @@
-// Import init to ensure templates are registered
-import '../init';
-
-// Re-export the page from the framework
-export { default, generateMetadata } from '@shopana/next-ecommerce-core/app/[[...slug]]/page';
--- src/app/demo-framework/init.ts
+++
@@ -1,6 +0,0 @@
-import { registerDefaultTemplates } from '@shopana/next-ecommerce-core';
-
-// Register default templates on module load
-registerDefaultTemplates();
-
-export const templatesRegistered = true;
